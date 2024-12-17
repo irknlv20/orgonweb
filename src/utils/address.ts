a6ddddb13ede1053ae717c2f7fa7cc1a7ab241e2
aozr <aozerov@gmail.com>
2024-12-17 15:33:19 +0300

    41 => 73

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -4,9 +4,9 @@ import { isHex, isString } from './validations.js';
 import { keccak256 } from './ethersUtils.js';
 
 export const ADDRESS_SIZE = 34;
-export const ADDRESS_PREFIX = '41';
-export const ADDRESS_PREFIX_BYTE = 0x41;
-export const ADDRESS_PREFIX_REGEX = /^(41)/;
+export const ADDRESS_PREFIX = '73';
+export const ADDRESS_PREFIX_BYTE = 0x73;
+export const ADDRESS_PREFIX_REGEX = /^(73)/;
 
 export const TRON_BIP39_PATH_PREFIX = "m/44'/195'";
 export const TRON_BIP39_PATH_INDEX_0 = TRON_BIP39_PATH_PREFIX + "/0'/0/0";
